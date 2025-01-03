74438c8e21fe6acf9326c999a3e3c9fddb332a6f
cisse410 <dramisse90@gmail.com>
2025-01-03 22:49:27 +0000

    ajout d'un skeleton loader

---
+++ src/app/components/product-card-skeleton/product-card-skeleton.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card-skeleton',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-card-skeleton.component.html',
+  styleUrl: './product-card-skeleton.component.css',
+})
+export class ProductCardSkeletonComponent {
+  @Input() items!: number[];
+}
--- src/app/pages/product/product.page.ts
+++ src/app/pages/product/product.page.ts
@@ -1,18 +1,34 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductCardSkeletonComponent } from '../../components/product-card-skeleton/product-card-skeleton.component';
 import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [ProductCardComponent, AsyncPipe],
+  imports: [ProductCardComponent, ProductCardSkeletonComponent],
   templateUrl: './product.page.html',
   styleUrl: './product.page.css',
 })
 // eslint-disable-next-line @angular-eslint/component-class-suffix
-export class ProductPage {
+export class ProductPage implements OnInit {
   productSerrvice = inject(ProductService);
+  products: Product[] = [];
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  isLoading = true;
 
-  product$ = this.productSerrvice.getProducts();
+  ngOnInit() {
+    this.productSerrvice.getProducts().subscribe({
+      next: (response) => {
+        this.products = response;
+        this.isLoading = false;
+      },
+      error: () => {
+        this.isLoading = false; // Handle error case
+      },
+    });
+  }
+
+  // product$ = this.productSerrvice.getProducts();
 }
