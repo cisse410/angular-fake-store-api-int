ee24ab12a87fb9563d780fa2303fae5b0886eee0
cisse410 <dramisse90@gmail.com>
2025-01-13 22:54:16 +0000

    ajout d'un navbar

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css',
 })
